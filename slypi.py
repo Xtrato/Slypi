5284cb74bd7e8f9ab0d2ba09650a5fd904e43de4
Xtrato <james.woolley5@googlemail.com>
2013-03-27 15:22:26 +0000

    The baseline for the initial 7 modules have beed added. Inline sniffer, Printer, ARP spoof, ReverseSSH, Deauth Flood, Upsidedown-tnet, and nmap scan / upload. The menu has also been added to include the first 7 initial modules.

--- slypi.py
+++ slypi.py
@@ -7,12 +7,61 @@ from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 # pass '0' for early 256 MB Model B boards or '1' for all later versions
 lcd = Adafruit_CharLCDPlate(busnum = 0)
 
-# Clear display and show greeting, pause 1 sec
+# The following are the modules which can be added the SlyPi Device.
+# To add the module simply define the function below and then add the module name and function name to the modules dictonary.
+# All code the module executes is to be placed in the function.
+def inlineSniffer():
+    print "Inline Sniffer"
+    
+def printer():
+    print "Mass Network Printer"
+    
+def aprSpoof():
+    print "ARP Cache Poisoning"
+
+def reverseSSH():
+    print 'Reverse SSH'
+
+def deauthFlood():
+    print 'Deauth Flood'
+    
+def upsidedown():
+    print 'Upside Down internet'
+    
+def nmapScanUpload():
+    print 'Nmap Scan & Upload'  
+
+#Contains all modules which can be run on the device. The key is the displayed name on the LCD and the value is the function name
+modules = {'Inline Ethernet\nSniffer': 'inlineSniffer', 'Mass Network\nPrinter': 'printer', 'ARP Cache\nSpoofing': 'aprSpoof', 'Reverse SSH\nTunnel': 'reverseSSH', 'Deauth\nFlooding': 'deauthFlood', 'Upside-Down\nInternet': 'upsidedown', 'NMAP Scan\nand Upload': 'nmapScanUpload'}
+displayText = modules.keys()
+# Clears the display
 lcd.clear()
-lcd.message("James\nWoolley")
-col = (lcd.RED , lcd.YELLOW, lcd.GREEN, lcd.TEAL,
-       lcd.BLUE, lcd.VIOLET, lcd.ON   , lcd.OFF)
-while  True:
-  for c in col:
-      lcd.backlight(c)
-      sleep(.5)
+#Used to control the flow through the menu
+menuOption = 0
+lcd.backlight(lcd.BLUE)
+lcd.message("SlyPi\nPress Select")
+
+#The following while loop controls the LCD menu and the control using the keypad through the menu.
+while True:
+    if lcd.buttonPressed(lcd.SELECT):
+        sleep(0.5)
+        lcd.clear()
+        lcd.message(displayText[menuOption])
+        while True:
+            if lcd.buttonPressed(lcd.DOWN):
+                menuOption = menuOption + 1
+                if menuOption > len(modules) - 1:
+                    menuOption = 0
+                lcd.clear()
+                lcd.message(displayText[menuOption])
+                sleep(0.5)
+            if lcd.buttonPressed(lcd.UP):
+                menuOption = menuOption - 1
+                if menuOption < 0:
+                    menuOption = len(modules) - 1
+                lcd.clear()
+                lcd.message(displayText[menuOption])
+                sleep(0.5)
+            if lcd.buttonPressed(lcd.SELECT):
+                globals().get(modules[displayText[menuOption]])()
+                sleep(0.5)                 
