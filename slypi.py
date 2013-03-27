e0ccedd711896d47c95e729269886f6fc2e19e0d
Xtrato <james.woolley5@googlemail.com>
2013-03-27 10:45:49 +0000

    test functionality

--- slypi.py
+++ slypi.py
@@ -1,1 +1,18 @@
-print "Coming soon!"
+#!/usr/bin/python
+
+from time import sleep
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
+# Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
+# pass '0' for early 256 MB Model B boards or '1' for all later versions
+lcd = Adafruit_CharLCDPlate(busnum = 0)
+
+# Clear display and show greeting, pause 1 sec
+lcd.clear()
+lcd.message("James\nWoolley")
+col = (lcd.RED , lcd.YELLOW, lcd.GREEN, lcd.TEAL,
+       lcd.BLUE, lcd.VIOLET, lcd.ON   , lcd.OFF)
+while  True:
+  for c in col:
+      lcd.backlight(c)
+      sleep(.5)
